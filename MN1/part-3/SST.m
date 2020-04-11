c9342e5ea0161b37a418e99b4de30fef44dda390
Tudor Pescaru <tudorp00@gmail.com>
2020-04-11 18:38:17 +0300

    Updates codingstyle for part 3 of assignment 1

--- MN1/part-3/SST.m
+++ MN1/part-3/SST.m
@@ -1,17 +1,17 @@
-function [x] = SST(A, b)
-  % check if matrix A is upper triangular
-  if max(max(abs(tril(A, -1)))) > 10^3 * eps
+function [x] = SST (A, b)
+  ## check if matrix A is upper triangular
+  if max (max (abs (tril (A, -1)))) > 10^3 * eps
     x = NaN;
     return;
   endif
-  % calculate solution vector from back to front
-  [n, m] = size(A);
-  % solution vector will have size = nr of columns in A
-  % this is done to accommodate non-square matrices
-  x = zeros(m, 1);
-  % calculate last element first
+  ## calculate solution vector from back to front
+  [n, m] = size (A);
+  ## solution vector will have size = nr of columns in A
+  ## this is done to accommodate non-square matrices
+  x = zeros (m, 1);
+  ## calculate last element first
   x(m, 1) = b(m, 1) / A(m, m);
-  % itteratively calculate new elements based on previous ones
+  ## itteratively calculate new elements based on previous ones
   for k = (m - 1):-1:1
     sigma = A(k, ((k + 1) : m)) * x(((k + 1) : m), 1);
     x(k, 1) = (b(k, 1) - sigma) / A(k, k);
--- MN1/part-3/hsvHistogram.m
+++ MN1/part-3/hsvHistogram.m
@@ -1,26 +1,26 @@
-function [sol] = hsvHistogram(path_to_image, count_bins)
-  % read image data from path 
-  rgb_image = imread(path_to_image);
-  % convert rgb to hsv
-  hsv_image = RGB2HSV(rgb_image);
-  % separate hsv-coverted image into H, S and V matrices
+function [sol] = hsvHistogram (path_to_image, count_bins)
+  ## read image data from path 
+  rgb_image = imread (path_to_image);
+  ## convert rgb to hsv
+  hsv_image = RGB2HSV (rgb_image);
+  ## separate hsv-coverted image into H, S and V matrices
   H = hsv_image(:, :, 1);
   S = hsv_image(:, :, 2);
   V = hsv_image(:, :, 3);
-  % initialise sol vector and bin ranges
-  sol = zeros(1, 3 * count_bins); 
+  ## initialise sol vector and bin ranges
+  sol = zeros (1, 3 * count_bins); 
   binrange = [0:(101 / count_bins):101];
   binrange = binrange / 100;
-  % create histogram for hue
-  counts = histc(H, binrange);
+  ## create histogram for hue
+  counts = histc (H, binrange);
   counts = counts((1 : count_bins), :);
-  sol(1:count_bins) = sum(counts');
-  % create histogram for saturation
-  counts = histc(S, binrange);
+  sol(1:count_bins) = sum (counts');
+  ## create histogram for saturation
+  counts = histc (S, binrange);
   counts = counts((1 : count_bins), :);
-  sol((count_bins + 1):(2 * count_bins)) = sum(counts');
-  % create histogram for value
-  counts = histc(V, binrange);
+  sol((count_bins + 1):(2 * count_bins)) = sum (counts');
+  ## create histogram for value
+  counts = histc (V, binrange);
   counts = counts((1 : count_bins), :);
-  sol((2 * count_bins + 1):(3 * count_bins)) = sum(counts');
+  sol((2 * count_bins + 1):(3 * count_bins)) = sum (counts');
 endfunction
